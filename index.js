f5730dfbc3c082c21c5bb2df1422c3f70b802938
adzman <35321056+adzman@users.noreply.github.com>
2022-04-29 15:54:21 +0000

    Bot up to date

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-const mySecret = process.env['TOKEN']
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+const token = process.env['TOKEN']
+const CLIENT_ID = process.env['client_id']
+const GUILD_ID = process.env['guild_id']
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong")
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}]; 
+console.log(CLIENT_ID)
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
   }
-})
-
-client.login(process.env['TOKEN'])
-
+})();
